d02c4c9179ba5a421ce686d0392e14a618c37762
Muniba Rahman <muniba.rahman02@gmail.com>
2025-04-06 16:39:43 +0500

    seller form functional + tested

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SellerAuthComponent } from './components/seller-auth/seller-auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent},
+    { path: "seller-auth", component: SellerAuthComponent},
+    { path: "cart", component: HomeComponent},
+    { path: "login", component: HomeComponent},
+];
